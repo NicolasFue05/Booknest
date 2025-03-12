f3a06cd5fb93e556a7bc5522ae5ac6d847f0c57d
NicolasFue05 <nicolasjfc05@gmail.com>
2025-03-11 19:09:26 -0500

    feat: :fire: Is library added successfully | Library page path resolved | Solved x gap in book title lenght | formatTItle

--- src/components/ui/book/bookInfo.jsx
+++ src/components/ui/book/bookInfo.jsx
@@ -1,17 +1,38 @@
 import { useState } from 'react'
 import BookButton from '../features/buttons/bookButton'
 import Tag from '../features/tags/tag'
+import PageProgress from '../features/progress/currentPage'
 
 export default function BookInformation({ book, closeBookInfo }) {
   const [isClosed, setIsClosed] = useState(false)
+
+  const parseBookProgress = {
+    book: {
+      ISBN: book.book.ISBN,
+      title: book.book.title,
+      author: book.book.author.name,
+      synopsis: book.book.synopsis,
+      year: book.book.year,
+      pages: book.book.pages,
+      genre: book.book.genre,
+      pageProgress: 0,
+      capProgress: 0,
+    },
+  }
+
+  console.log(parseBookProgress)
 
   const handleCloseClick = () => {
     setIsClosed(true)
     closeBookInfo()
   }
 
-  const formatTitle = (title) => (title.length > 22 ? 'h-25' : 'h-13 mb-2')
+  // library
+  const isLibraryPage = window.location.href.endsWith('library')
+  console.log(isLibraryPage)
 
+  const isHigher = book.book.title.length >= 25 ? 'gap-x-12' : ''
+  const formatTitle = (title) => (title.length > 25 ? 'h-25' : 'h-13 mb-2')
   const isSaved = localStorage.getItem(book.book.ISBN) ? true : false
 
   return (
@@ -21,7 +42,7 @@ export default function BookInformation({ book, closeBookInfo }) {
         className={`${isClosed ? 'hidden' : 'absolute'} flex flex-col justify-center items-center`}
       >
         <section
-          className={`${isClosed ? 'hidden' : 'flex flex-row'} text-soft-white shadow-xl shadow-black h-170 w-5xl bg-dark-blue p-6 rounded-xl relative`}
+          className={`${isClosed ? 'hidden' : 'flex flex-row'} ${isHigher} text-soft-white shadow-xl shadow-black h-170 w-5xl bg-dark-blue p-6 rounded-xl relative`}
         >
           <div className="w-100 flex flex-col gap-2">
             <img
@@ -65,6 +86,8 @@ export default function BookInformation({ book, closeBookInfo }) {
               </p>
               <Tag Text={book.book.genre} />
             </div>
+
+            {isLibraryPage && <PageProgress bookProgress={parseBookProgress} />}
 
             {/* Mover el botón a la parte inferior derecha */}
             <div className="absolute bottom-8 right-8">
