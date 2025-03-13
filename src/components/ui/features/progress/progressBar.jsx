e824840ff0dab2f238320dbcbbf4c3711b92055b
NicolasFue05 <nicolasjfc05@gmail.com>
2025-03-13 15:33:23 -0500

    feat: :fire: Progress bar added succesfully

---
+++ src/components/ui/features/progress/progressBar.jsx
@@ -0,0 +1,17 @@
+export default function ProgressBar({ current, limit }){
+  const progress = (current / limit) * 100 // Calcula el porcentaje
+  const barWidth = (current / limit) * 220 // Calcula el ancho relativo a 220px
+
+  return (
+    <div className="flex flex-row gap-x-2 mt-4">
+      <span className="font-bold">Progress:</span>
+      <div className="bg-soft-blue rounded-xl border-blue-800 w-[220px] max-w-[220px] h-6">
+        <div
+          className="bg-blue-800 h-6 rounded-xl transition-all duration-300"
+          style={{ width: `${barWidth}px` }} // Ancho dinámico
+        ></div>
+      </div>
+      <span className="font-bold">{progress.toFixed(1)}%</span>
+    </div>
+  )
+}
